6889385eb4fbcbaa4489eb02aabf57660d12b31b
avulalakshman <learnwithlakshman@gmail.com>
2021-07-07 15:03:24 +0530

    Added cors filter;

--- src/main/java/com/heraizen/gpet/config/WebCORSConfig.java
+++ src/main/java/com/heraizen/gpet/config/WebCORSConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebCORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE");
     }
 }
